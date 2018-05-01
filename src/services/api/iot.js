cf56e0b477241c1a356f49756cde660c04814da9
vegter <amsjurien@gmail.com>
2018-05-01 11:52:40 +0200

    Connect to production API

--- src/services/api/iot.js
+++ src/services/api/iot.js
@@ -1,7 +1,6 @@
 import { readPaginatedData } from '@/services/datareader'
 
-const API = 'https://acc.api.data.amsterdam.nl'
-// const API = 'http://localhost:8000'
+const API = 'https://api.data.amsterdam.nl'
 
 let things = null
 let locations = null
